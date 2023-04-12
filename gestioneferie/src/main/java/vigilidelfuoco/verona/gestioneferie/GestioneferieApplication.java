1bef51f59f444922ddf25ee492a8022462b41069
milen <milen@Ale>
2023-04-12 18:50:05 +0200

    cors aggiunta

--- gestioneferie/src/main/java/vigilidelfuoco/verona/gestioneferie/GestioneferieApplication.java
+++ gestioneferie/src/main/java/vigilidelfuoco/verona/gestioneferie/GestioneferieApplication.java
@@ -1,7 +1,12 @@
 package vigilidelfuoco.verona.gestioneferie;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class GestioneferieApplication {
@@ -10,4 +15,26 @@ public class GestioneferieApplication {
 		SpringApplication.run(GestioneferieApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration () ;
+		corsConfiguration.setAllowCredentials(true) ;
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin",
+		"Access-Control-Allow-Origin", "Content-Type",
+		"Accept", "Authorization",
+		"Origin, Accept",
+		"X-Requested-With",
+		"Access-Control-Request-Method",
+		"Access-Control-Request-Headers"));
+		corsConfiguration. setExposedHeaders (Arrays.asList ("Origin", "Content-Type",
+		"Accept", "Authorization",
+		"Access-Control-Allow-Origin",
+		"Access-Control-Allow-Origin",
+		"Access-Control-ALlow-Credentials"));
+		corsConfiguration.setAllowedMethods (Arrays.asList ("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource=new UrlBasedCorsConfigurationSource(); 
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource) ;
+	 }
 }
