a69f01f1b699bb1c9b500454a37aeb8f20bedc07
milen <milen@Ale>
2023-04-14 14:51:42 +0200

    sistemato il configuration security

--- gestioneferie/src/main/java/vigilidelfuoco/verona/gestioneferie/configurazioneSpringSecurity/WebSecurityConfiguration.java
+++ gestioneferie/src/main/java/vigilidelfuoco/verona/gestioneferie/configurazioneSpringSecurity/WebSecurityConfiguration.java
@@ -3,12 +3,18 @@ package vigilidelfuoco.verona.gestioneferie.configurazioneSpringSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -18,15 +24,46 @@ public class WebSecurityConfiguration  {
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
+//	@Bean
+//	AuthenticationProvider authenticationProvider() {
+//		
+//		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//		provider.setUserDetailsService(userDetailsService);
+//		provider.setPasswordEncoder(new BCryptPasswordEncoder());
+//		return provider;
+//		
+//	}
+	
 	@Bean
-	AuthenticationProvider authenticationProvider() {
-		
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(new BCryptPasswordEncoder());
-		return provider;
-		
+	AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsService userDetailsService) 
+	  throws Exception {
+	    return http.getSharedObject(AuthenticationManagerBuilder.class)
+	      .userDetailsService(userDetailsService)
+	      .passwordEncoder(bCryptPasswordEncoder)
+	      .and()
+	      .build();
 	}
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/home").hasAuthority("UTENTE")
+                .requestMatchers("/admin").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                .and().httpBasic(withDefaults());
+        
+        return http.build();
+		
+//        .formLogin(formLogin ->
+//            formLogin
+//                .loginPage("/login")
+//                .permitAll()
+//        )
+//        .rememberMe(withDefaults());
+		}
+	
 	
 
 }
