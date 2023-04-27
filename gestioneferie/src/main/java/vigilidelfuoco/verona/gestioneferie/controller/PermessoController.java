0d547d3a45b87e79407a4550e67d1ef1b126eb5b
ilenia.mannino <ilenia.mannino@VERONA-122.vr.dipvvf.it>
2023-04-27 15:47:23 +0200

    mi sono dimenticata permesso controller

--- gestioneferie/src/main/java/vigilidelfuoco/verona/gestioneferie/controller/PermessoController.java
+++ gestioneferie/src/main/java/vigilidelfuoco/verona/gestioneferie/controller/PermessoController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +38,17 @@ public class PermessoController {
 		Permesso newPermesso= permessoService.aggiungiPermesso(permesso);
 		return new ResponseEntity<>(newPermesso, HttpStatus.CREATED);
 	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<?> cancellaPermesso(@PathVariable("id") Long id){
+		permessoService.deletePermessoById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+//	@GetMapping("/find/{idUtente}")
+//	public ResponseEntity<List<Permesso>> findPermessiByIdUtente(@PathVariable("idUtente") Long id){
+//		List<Permesso> permessi = permessoService.findPermessiByIdUtente(id);
+//		return new ResponseEntity<>(permessi, HttpStatus.OK);
+//	}
 	
 }
