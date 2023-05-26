e919763701b1305109ad006c31d4eb21145a0288
aleniaa <mannilenia@gmail.com>
2023-05-27 01:36:13 +0200

    piccole modifiche

--- gestioneferie/src/main/java/vigilidelfuoco/verona/gestioneferie/controller/WelcomeController.java
+++ gestioneferie/src/main/java/vigilidelfuoco/verona/gestioneferie/controller/WelcomeController.java
@@ -4,10 +4,13 @@ package vigilidelfuoco.verona.gestioneferie.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import vigilidelfuoco.verona.gestioneferie.model.Permesso;
 import vigilidelfuoco.verona.gestioneferie.model.Utente;
 import vigilidelfuoco.verona.gestioneferie.service.LoginService;
 
@@ -55,7 +58,17 @@ public class WelcomeController {
 		
 		//return new ResponseEntity<>(utenteLoggato, HttpStatus.OK);
 		
-}
+	}
+	
+	@PutMapping("/checkPass")
+		public ResponseEntity<String> aggiungiPermesso(@RequestParam("password") String password){
+
+		
+		//boolean isDefaultPass = loginService.checkPass(password);
+
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Password da cambiare");
+
+	}
 }
 
 
