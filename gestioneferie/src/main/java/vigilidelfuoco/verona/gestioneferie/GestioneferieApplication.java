ec7dcffb86a9380d56ac158ee18a85fe9e68fec4
ilenia.mannino <ilenia.mannino@VERONA-122.vr.dipvvf.it>
2024-03-15 15:28:08 +0100

    aggiunta qualcosa per il deploy

--- gestioneferie/src/main/java/vigilidelfuoco/verona/gestioneferie/GestioneferieApplication.java
+++ gestioneferie/src/main/java/vigilidelfuoco/verona/gestioneferie/GestioneferieApplication.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -11,8 +13,13 @@ import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 
-public class GestioneferieApplication {
+public class GestioneferieApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(GestioneferieApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GestioneferieApplication.class, args);
 	}
